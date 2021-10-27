07a204673790bdfdd778af7808cfcaa88165d7d0
yevheniidits <yevheniidits@gmail.com>
2021-10-27 13:39:01 +0300

    test commit

--- task.py
+++ task.py
@@ -1,7 +1,17 @@
-def test_task():
-    print('Hello world!')
-    return
+from RPA.Browser.Selenium import Selenium
+
+
+class InsertDataAndExportPdf(object):
+
+    def __init__(self):
+        self.wd = Selenium()
+
+    def open_intranet_website(self):
+        self.wd.open_available_browser(url='https://robotsparebinindustries.com/')
+        print(self.wd.get_cookies())
+        return
 
 
 if __name__ == '__main__':
-    test_task()
+    task = InsertDataAndExportPdf()
+    task.open_intranet_website()
--- task_2.py
+++
@@ -1,7 +0,0 @@
-def test_task():
-    print('Hello world! Again!!!')
-    return
-
-
-if __name__ == '__main__':
-    test_task()
