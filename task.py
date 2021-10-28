a7d049d7e07f62e295b5aba0aca56f0895dcbc24
yevheniidits <yevheniidits@gmail.com>
2021-10-28 09:07:57 +0300

    test commit

--- task.py
+++ task.py
@@ -1,89 +1,125 @@
-from pathlib import Path
-
 from RPA.Browser.Selenium import Selenium
-from RPA.HTTP import HTTP
-from RPA.Excel.Files import Files
+from RPA.Excel.Files import XlsxWorkbook
 from RPA.PDF import PDF
-from RPA.Robocorp.Vault import Vault
+import os
+import time
+import re
 
 
-class InsertDataAndExportPdf(object):
-
-    def __init__(self):
-        self.wd = Selenium()
-        self.http = HTTP()
-        self.excel = Files()
-        self.pdf = PDF()
-        self.output_dir = Path(Path.cwd(), 'output')
-        secrets = Vault().get_secret('credentials')
-        self.username = secrets['username']
-        self.password = secrets['password']
-
-    def run_task(self):
-        try:
-            self.open_intranet_website()
-            self.log_in()
-            self.download_excel_file()
-            self.fill_form_with_data_from_excel_file()
-            self.collect_results()
-            self.export_table_as_pdf()
-        finally:
-            self.logout_and_close_browser()
-        return
-
-    def open_intranet_website(self):
-        self.wd.open_available_browser(url='https://robotsparebinindustries.com/')
-        return
-
-    def log_in(self):
-        self.wd.input_text('username', self.username)
-        self.wd.input_text('password', self.password)
-        self.wd.submit_form()
-        return
-
-    def download_excel_file(self):
-        self.http.download(url='https://robotsparebinindustries.com/SalesData.xlsx', overwrite=True)
-        return
-
-    def fill_form_with_data_from_excel_file(self):
-        self.excel.open_workbook('SalesData.xlsx')
-        sales_reps = self.excel.read_worksheet(header=True)
-        self.excel.close_workbook()
-        for person in sales_reps:
-            self.fill_and_submit_form_for_one_person(person)
-        return
-
-    def fill_and_submit_form_for_one_person(self, person):
-        try:
-            self.wd.wait_until_page_contains_element('sales-form')
-            self.wd.input_text('firstname', person['First Name'])
-            self.wd.input_text('lastname', person['Last Name'])
-            self.wd.input_text('salesresult', person['Sales'])
-            self.wd.select_from_list_by_value('salestarget', str(person['Sales Target']))
-            self.wd.submit_form('sales-form')
-        except TypeError:
-            pass
-        return
-
-    def collect_results(self):
-        file_path = str(Path(self.output_dir, 'sales_summary.png'))
-        self.wd.screenshot('css:div.sales-summary', file_path)
-        return
-
-    def export_table_as_pdf(self):
-        self.wd.wait_until_element_is_visible('sales-results')
-        sales_result_html = self.wd.get_element_attribute('sales-results', 'outerHTML')
-        file_path = str(Path(self.output_dir, 'output.pdf'))
-        self.pdf.html_to_pdf(sales_result_html, file_path)
-        return
-
-    def logout_and_close_browser(self):
-        self.wd.click_button('logout')
-        self.wd.wait_until_page_does_not_contain_element('logout')
-        self.wd.close_browser()
-        return
+excel_book = XlsxWorkbook('output/agencies.xlsx')
+excel_book.create()
 
 
-if __name__ == '__main__':
-    task = InsertDataAndExportPdf()
-    task.run_task()
+class PDFLib(object):
+
+    def __init__(self, pdf_obj):
+        self.pdf = pdf_obj
+
+    def pdf_data(self, name: str) -> list:
+        text_pdf = self.pdf.get_text_from_pdf('output/' + name)
+        text = text_pdf[1].replace('\n', ' ')
+        name_inv_re = re.search(r'Name of this Investment: (.*)2\. Unique', text)
+        name_inv = name_inv_re.group(1) if name_inv_re else ''
+        uii_re = re.search(r'Unique Investment Identifier \(UII\): (.*)Section B', text)
+        uii = uii_re.group(1) if uii_re else ''
+        return [uii, name_inv]
+
+
+class Driver(object):
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def wait_elements(self, css_selector: str, timeout: int = 10):
+        """Wait and find elements with css selector.
+        Wait timeout(def 10s)"""
+        self.browser.wait_until_element_is_visible(f'css:{css_selector}', timeout)
+        return self.browser.find_elements(f'css:{css_selector}')
+
+    def parse_info(self):
+        self.wait_elements('.dataTables_scrollHeadInner th')
+
+        self.browser.select_from_list_by_value('css:select[name=investments-table-object_length]', '-1')
+        self.browser.wait_until_element_is_enabled('css:.paginate_button.next.disabled', 20)
+        all_records = self.wait_elements('#investments-table-object tbody tr', 60)
+
+        details_investments = []
+        excel_book.create_worksheet('investments')
+        for tmp_record in all_records:
+            _record = tmp_record.find_elements_by_css_selector('td')
+            record = [i.text for i in _record]
+            details_investments.append(record)
+        excel_book.append_worksheet('investments', details_investments)
+
+        selector_links = self.browser.find_elements('css:#investments-table-object td a')
+        all_links = [i.get_attribute('href') for i in selector_links]
+        for link in all_links:
+            self.browser.go_to(link)
+            self.browser.wait_until_element_is_visible(f'css:#business-case-pdf a', 30)
+            self.browser.click_element('css:#business-case-pdf a')
+            self.browser.wait_until_element_is_not_visible('css:#business-case-pdf span', 60)
+            time.sleep(1)
+
+
+def main():
+    name_agencies = 'Department of Education'
+    browser = Selenium()
+    browser.set_download_directory(rf"{os.getcwd()}\\output")
+    web_driver = Driver(browser)
+    try:
+        browser.open_available_browser("http://itdashboard.gov/")
+        browser.click_element(f'css:.tuck-7 .trend_sans_oneregular.btn-default.btn-lg-2x')
+        agency_elements = web_driver.wait_elements('#agency-tiles-widget .tuck-5')
+        excel_book.rename_worksheet('agencies')
+
+        all_agencies = []
+        for agency in agency_elements:
+            agency_title = agency.find_element_by_css_selector('.h4.w200').text
+            agency_amount = agency.find_element_by_css_selector('.h1.w900').text
+            all_agencies.append([agency_title, agency_amount])
+
+        excel_book.append_worksheet('agencies', all_agencies)
+
+        for agency in agency_elements:
+            title = agency.find_element_by_css_selector('.h4.w200').text
+            if title == name_agencies:
+                agency.find_element_by_css_selector('.col-sm-12 a').click()
+                web_driver.parse_info()
+                break
+
+        excel_book.save()
+
+        # compare pdf and excel files
+        pdf = PDF()
+        pdf_lib = PDFLib(pdf)
+        all_investments = {}
+        investments_data = excel_book.read_worksheet('investments')
+        for investment in investments_data:
+            if investment.get('A'):
+                all_investments[investment['A']] = investment.get('C')
+
+        all_pdf_files = []
+
+        for f in os.listdir('output'):
+            name, ext = os.path.splitext(f)
+            if ext == '.pdf':
+                all_pdf_files.append(f)
+
+        for pdf in all_pdf_files:
+            data = pdf_lib.pdf_data(pdf)
+            if data[0] and data[1] == all_investments.get(data[0]):
+                print(f'is true:\n{data[0]} - {data[1]}')
+            else:
+                print(
+                    f"Does not match"
+                    f"PDF:\nUII: {data[0]}\nInvestments name:{data[1]}\n\n"
+                    f"Excel:\nInvestments name: {all_investments.get(data[0])}"
+                )
+
+    finally:
+        browser.close_all_browsers()
+        excel_book.close()
+
+
+if __name__ == "__main__":
+    main()
